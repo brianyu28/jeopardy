471c21f1d4a40b9a74f1713e3d09070e472ea1d6
Brian Yu <brianyu28@gmail.com>
2020-01-19 23:23:11 -0500

    Updates

---
+++ precache-manifest.6464c9561df3628d93bc809ec53ddf98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f47d418abc2dc9b9e0426c39536e993",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "dcef451ac77284e86fa4",
+    "url": "/jeopardy/static/css/main.2dc98972.chunk.css"
+  },
+  {
+    "revision": "3dab551063618d069426",
+    "url": "/jeopardy/static/js/2.016507e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jeopardy/static/js/2.016507e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcef451ac77284e86fa4",
+    "url": "/jeopardy/static/js/main.7b15037c.chunk.js"
+  },
+  {
+    "revision": "51f877cc240f2aa71235",
+    "url": "/jeopardy/static/js/runtime-main.795492d8.js"
+  },
+  {
+    "revision": "6433bfa5b068274f1392b5301958e8e2",
+    "url": "/jeopardy/static/media/Swiss921-BT_43276.6433bfa5.ttf"
+  },
+  {
+    "revision": "d5d36d20464aff2a1503056f6090b888",
+    "url": "/jeopardy/static/media/korinn.d5d36d20.ttf"
+  }
+]);
